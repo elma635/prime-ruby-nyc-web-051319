12b3f4ff134002f9c06daea51283e4ca152db85b
Elma Rahman <elmarahman635@gmail.com>
2019-05-10 04:14:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,16 +2,16 @@
 
 
 def prime?(integer)
-  is_prime = true
+  is_prime = true #defaulting it to true
  if integer <= 1
    return false
 else
 
-  for i in 2..integer-1
+  for i in 2..integer-1 #array of # -- i whatever #
     if integer % i == 0
       is_prime = false
     end
   end
    is_prime
 end
-end 
+end
